d488b5781ba9b13dca56cea602f561278a93be68
Olga Nadongo <winnienadongo@gmail.com>
2022-07-15 13:20:33 +0300

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,20 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+   const linkItems=links.map((link, index)=>{
+     return(
+      <a href={'#'+ link} key={index}>{link}</a>
+      )
+   })
+
+  return (
+    
+        <nav>
+        {/* display an <a> tag for each link here */}
+          {linkItems}
+           
+       </nav>
+      )
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  
+  const technologyItems= technologies.map((technology,index)=>{
+    return (
+    <span key={index}>{technology}</span>
+    )
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+          {technologyItems}
       </div>
     </div>
   );
